05f107bf76bdb86022d430fca37d007394c8e592
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-10-13 15:54:30 +0200

    Created page 'create' linked via link to the page 'comics.index'

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,11 +1,20 @@
 @extends('layout.main')
 
 @section('title', 'Comics')
-    
+@section('cdns')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
 
 @section('content')
 <section class="container">
-    <h2 class="text-center fw-bold py-2 text-primary">Comics</h2>
+    <div class="d-flex justify-content-between align-items-center me-4">
+        <h2 class="text-center fw-bold py-2 text-primary">Comics</h2>
+        <button class="btn btn-secondary">
+            <a class="text-white text-decoration-none" href="{{route('comics.create')}}">
+                <i class="fas fa-plus"></i>
+            </a>
+        </button>
+    </div>
     <div class="row justify-content-evenly my-4">
 
         @forelse ($comics as $comic)
@@ -15,9 +24,7 @@
                 <h5 class="card-title">{{$comic->title}}</h5>
                 <a href="{{route('comics.show', $comic->id)}}" class="btn btn-primary">View more info</a>
                 </div>
-            </div>    
-
-            
+            </div>          
         @empty
         <h3 class="text-center">Fumetti non disponibili</h3>       
         @endforelse
